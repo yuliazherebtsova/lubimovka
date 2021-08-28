0ec83a01388dd03b72492cf65e31dcfac17bd240
Yulia Zherebtsova <juliaz1206@yandex.ru>
2021-08-28 04:26:03 +0300

    feat: завершение верстки под desktop, подгрузка отзывов из заготовленого массива, работа с буллетами

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,25 @@
 /*
+TODO Чек-лист [V]
+
 * 1. Массив исходных отзывов, подгружаемых из template
 
-* 2. Стилизовать стандартные контролы (повторить тренажер?)
+* 2. Динамическое количеcтво буллетов
 
-* 3. Анимация на кнопках слайдера
+* 3. Анимация, ховеры: https://codepen.io/spacehaz/pen/VwWLGqo
 
-* 4. Учесть размер текстов и разное количество отзывов
+* 4. Кнопка "Читать полностью": https://yandex-students.slack.com/archives/C021JDQ27N2/p1630001802024100
 
-* 5. Грузить swiper через npm
+* 5. Моб. верстка (в видео по  Swiper есть)
 
-* 6. Досмотреть видео по swiper
-
-* 7. Сделать одну кнопку стрелок с друмя модификаторами
+* 6. БЭМ + HTML Валидатор
 
 */
 
-// Инициализируем Swiper
-const swiperFeedback = new Swiper('.feedback__slider', {
-  wrapperClass: 'feedback__wrapper',
-  slideClass: 'feedback__slide',
-  slideActiveClass: 'feedback__slide_type_active',
-  //slideVisibleClass: 'feedback__slide_type_visible',
+// Инициализируем Swiper для блока Reviews
+const swiperReviews = new Swiper('.reviews__container', {
+  wrapperClass: 'reviews__wrapper',
+  slideClass: 'reviews__slide',
+  slideActiveClass: 'slider__slide_type_active',
 
   // Отступ между слайдами
   spaceBetween: 50,
@@ -50,8 +49,7 @@ const swiperFeedback = new Swiper('.feedback__slider', {
   // Активный слайд по центру
   centeredSlides: true,
 
-
-  initialSlide: 1,
+  initialSlide: 0,
   slideToClickedSlide: true,
   watchSlidesVisibility: true,
 
@@ -66,23 +64,21 @@ const swiperFeedback = new Swiper('.feedback__slider', {
 
   // Стрелки
   navigation: {
-    nextEl: '.feedback__next-button',
-    prevEl: '.feedback__prev-button'
+    nextEl: '.reviews__next-button',
+    prevEl: '.reviews__prev-button'
   },
 
   // Навигация
   // Буллеты, текущее положение
   pagination: {
-    el: '.feedback__pagination',
-    bulletClass: 'feedback__bullet',
-    bulletActiveClass: 'feedback__bullet_type_active',
+    el: '.reviews__pagination',
+    bulletClass: 'slider__bullet',
+    bulletActiveClass: 'slider__bullet_type_active',
     // Буллеты
     clickable: true,
     // dynamicBullets: true,
     // dynamicMainBullets: 4,
   },
-
-
 
   // Включение / отключение перетаскивания на десктопе
   simulateTouch: true,
@@ -120,3 +116,136 @@ const swiperFeedback = new Swiper('.feedback__slider', {
 
   slideToClickedSlide: true
 });
+
+
+// Инициализируем Swiper для блока Feedback
+const swiperFeedback = new Swiper('.feedback__container', {
+  wrapperClass: 'feedback__wrapper',
+  slideClass: 'feedback__slide',
+
+  // Отступ между слайдами
+  spaceBetween: 30,
+
+  // Кол-во отображаемых слайдов
+  slidesPerView: 3,
+
+  // Кол-во пролистываемых слайдов
+  slidesPerGroup: 1,
+
+  // Активный слайд по центру
+  centeredSlides: false,
+
+  // Листать бесконечно
+  loop: true,
+
+  // Свободный режим прокрутки
+  freeMode: true,
+
+  // Автопрокрутка
+  // autoplay: {
+  //   // Пауза между прокруткой
+  //   delay: 3000,
+  //   disableOnIteraction: false
+  // },
+
+  // Скорость переключения
+  speed: 1000,
+
+  initialSlide: 0,
+  slideToClickedSlide: true,
+  watchSlidesVisibility: true,
+
+  // Эффекты переключения слайдов
+  // // смена прозрачности
+  // effect: 'fade',
+  // // Дополнение к fade
+  // fadeEffect: {
+  //   // Параллельная смена прозрачности
+  //   crossFade: true
+  // },
+
+  // Стрелки
+  navigation: {
+    nextEl: '.feedback__next-button',
+    prevEl: '.feedback__prev-button'
+  },
+
+  // Навигация
+  // Буллеты, текущее положение
+  pagination: {
+    el: '.feedback__pagination',
+    bulletClass: 'slider__bullet',
+    bulletActiveClass: 'slider__bullet_type_active',
+    // Буллеты
+    clickable: true,
+    // dynamicBullets: true,
+    // dynamicMainBullets: 4,
+  },
+  watchOverflow: true,
+
+  // Включение / отключение перетаскивания на десктопе
+  simulateTouch: true,
+
+  // Чувствительность свайпа
+  touchRatio: 1,
+
+  // Угол срабатывания свайпа
+  touchAngle: 45,
+
+  // Курсор перетаскивания
+  grabCoursor: true,
+
+  // Навигация по хэшу
+  hashNavigation: {
+    // Отслеживать состояние
+    watchState: true,
+  },
+
+  // Брейкпойнты для адаптива
+
+  breakpoints: {
+    320: {
+      slidedPerView: 1,
+    },
+    480: {
+      slidedPerView: 2,
+    },
+    922: {
+      slidedPerView: 3,
+    }
+  },
+
+  lazyLoading: true,
+
+  slideToClickedSlide: true,
+});
+
+//const sliderFeedback = document.querySelector('.feedback__wrapper');
+
+function createCard(cardData) {
+  // функция создания карточки
+  const cardTemplate = document.querySelector('#slideFeedback-template').content;
+  const cardElement = cardTemplate.querySelector('.feedback__slide').cloneNode(true);
+  // создаем новую карточку по шаблону
+  cardElement.querySelector('.card__text').textContent = cardData.text;
+  cardElement.querySelector('.card__author').textContent = cardData.author;
+
+  return cardElement;
+}
+
+function renderCard(card) {
+  // функция добавления новой карточки в разметку
+  swiperFeedback.appendSlide(card);
+}
+
+function loadInitialCards(cards) {
+  // функция загрузки начальных карточек на страницу
+  cards.forEach(element => {
+    const newCard = createCard(element);
+    renderCard(newCard);
+  });
+};
+
+loadInitialCards(initialCardsFeedback);
+// при загрузке страницы загружаем карточки из заранее заготовленного массива
+swiperFeedback.slideReset()
