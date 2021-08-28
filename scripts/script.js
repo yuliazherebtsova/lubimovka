48766b9d7d60298fd3ea08bae5defa78a5919924
Yulia Zherebtsova <juliaz1206@yandex.ru>
2021-08-28 23:16:47 +0300

    refactor: очистка от кода, касающегося блока со вторым слайдером, последние мелкие исправления

--- scripts/script.js
+++ scripts/script.js
@@ -1,96 +1,3 @@
-/*
-TODO Чек-лист [V]
-
-* 1. Массив исходных отзывов, подгружаемых из template
-
-* 2. Анимация, ховеры: https://codepen.io/spacehaz/pen/VwWLGqo
-
-* 3. Кнопка "Читать полностью": https://yandex-students.slack.com/archives/C021JDQ27N2/p1630001802024100
-
-* 4. Моб. верстка (в видео по  Swiper есть)
-
-* 5. БЭМ + HTML Валидатор
-
-*/
-
-// Инициализируем Swiper для блока Reviews
-const swiperReviews = new Swiper('.reviews__container', {
-  wrapperClass: 'reviews__wrapper',
-  slideClass: 'reviews__slide',
-  slideActiveClass: 'slider__slide_type_active',
-
-  // Отступ между слайдами
-  spaceBetween: 50,
-
-  // Кол-во отображаемых слайдов
-  slidesPerView: 2,
-
-  // Кол-во пролистываемых слайдов
-  slidesPerGroup: 1,
-
-  // Листать стрелками бесконечно
-  loop: true,
-
-  // Автопрокрутка
-  // autoplay: {
-  //   // Пауза между прокруткой
-  //   delay: 3000,
-  //   disableOnIteraction: true
-  // },
-
-  // Скорость переключения
-  speed: 1000,
-
-  // Активный слайд по центру
-  centeredSlides: true,
-
-  // Индекс активного слайда
-  initialSlide: 0,
-
-  // Переход по слайдам путем клика на следующий
-  slideToClickedSlide: true,
-
-  // Стрелки
-  navigation: {
-    nextEl: '.reviews__next-button',
-    prevEl: '.reviews__prev-button'
-  },
-
-  // Навигация
-  // Буллеты, текущее положение
-  pagination: {
-    el: '.reviews__pagination',
-    bulletClass: 'slider__bullet',
-    bulletActiveClass: 'slider__bullet_type_active',
-    // Буллеты
-    clickable: true,
-    dynamicBullets: true,
-    dynamicMainBullets: 4,
-  },
-
-  // Включение / отключение перетаскивания на десктопе
-  simulateTouch: false,
-
-  // Чувствительность свайпа
-  touchRatio: 1,
-
-  // Угол срабатывания свайпа
-  touchAngle: 45,
-
-  // Брейкпойнты для адаптива
-  breakpoints: {
-    320: {
-      slidedPerView: 1,
-    },
-    480: {
-      slidedPerView: 2,
-    },
-    922: {
-      slidedPerView: 3,
-    }
-  },
-});
-
 // Инициализируем Swiper для блока Feedback
 const swiperFeedback = new Swiper('.feedback__container', {
   wrapperClass: 'feedback__wrapper',
@@ -113,8 +20,6 @@ const swiperFeedback = new Swiper('.feedback__container', {
     bulletClass: 'slider__bullet',
     bulletActiveClass: 'slider__bullet_type_active',
     clickable: true,
-    dynamicBullets: true,
-    dynamicMainBullets: 4,
   },
   watchOverflow: true,
   simulateTouch: true,
@@ -158,3 +63,4 @@ function loadInitialCards(cards) {
 };
 
 loadInitialCards(initialCardsFeedback);
+swiperFeedback.slideNext(0) // Старт с первого слайда после загрузки из массивва
